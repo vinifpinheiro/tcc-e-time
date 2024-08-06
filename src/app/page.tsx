d4be1e5859aaee6a70efac016c352e7d763d149f
Vinicius <viniciusfreitasp@hotmail.com>
2024-08-05 21:10:20 -0300

    adding tauri

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen w-full"></div>
+    <div className="flex items-center justify-center h-screen w-full">
+      TEST 
+    </div>
   );
 }
