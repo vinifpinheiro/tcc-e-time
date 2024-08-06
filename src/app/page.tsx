f0f9c287ab99801f033c5651d9c09d2427ff4c10
Vinicius <viniciusfreitasp@hotmail.com>
2024-08-06 20:59:49 -0300

    window size and position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { WindowSizeProvider } from "@/components/window-size-provider";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen w-full">
-      TEST 
-    </div>
+    <WindowSizeProvider>
+      <div className="flex items-center justify-center h-[100vh] w-full bg-black/30">
+        TEST
+      </div>
+    </WindowSizeProvider>
   );
 }
---
+++ src/components/window-size-provider.tsx
@@ -0,0 +1,36 @@
+"use client";
+import {
+  appWindow,
+  LogicalPosition,
+  LogicalSize,
+  currentMonitor,
+} from "@tauri-apps/api/window";
+import { useEffect } from "react";
+
+async function setWindowPositionAndSize() {
+  const screenSize = await currentMonitor();
+
+  if (screenSize) {
+    const { size, position, scaleFactor } = screenSize;
+    console.log("scaleFactor", scaleFactor);
+    const windowSize = new LogicalSize(500, size.height);
+    await appWindow.setSize(windowSize);
+
+    const x = position.x + size.width / 2.495 - windowSize.width / 2.495;
+    const y = position.y + size.height / 2.495 - windowSize.height / 2.495;
+    const windowPosition = new LogicalPosition(x, y);
+    await appWindow.setPosition(windowPosition);
+  }
+}
+
+export const WindowSizeProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  useEffect(() => {
+    setWindowPositionAndSize();
+  }, []);
+
+  return <div>{children}</div>;
+};
