677d7ae9e03f260e4aca38e1fce8b22b1a15f497
Vinicius <viniciusfreitasp@hotmail.com>
2024-08-09 20:16:20 -0300

    fix dev env

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,51 +11,51 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_positioner::init())
         .system_tray(SystemTray::new().with_menu(system_tray_menu))
-        .setup(|app| {
-            app.set_activation_policy(tauri::ActivationPolicy::Accessory);
-            app.listen_global("quit", |_| {
-                std::process::exit(0);
-            });
+        // .setup(|app| {
+        //     app.set_activation_policy(tauri::ActivationPolicy::Accessory);
+        //     app.listen_global("quit", |_| {
+        //         std::process::exit(0);
+        //     });
 
-            let window = app.get_window("main").unwrap();
+        //     let window = app.get_window("main").unwrap();
 
-            #[cfg(target_os = "macos")]
-            apply_vibrancy(
-                &window,
-                NSVisualEffectMaterial::Menu,
-                Some(NSVisualEffectState::Active),
-                Some(6.0),
-            )
-            .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+        //     #[cfg(target_os = "macos")]
+        //     apply_vibrancy(
+        //         &window,
+        //         NSVisualEffectMaterial::Menu,
+        //         Some(NSVisualEffectState::Active),
+        //         Some(6.0),
+        //     )
+        //     .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
-            Ok(())
-        })
-        .on_system_tray_event(|app, event| {
-            tauri_plugin_positioner::on_tray_event(app, &event);
-            match event {
-                SystemTrayEvent::LeftClick {
-                    position: _,
-                    size: _,
-                    ..
-                } => {
-                    let window = app.get_window("main").unwrap();
-                    let _ = window.move_window(Position::TrayCenter);
-                    if window.is_visible().unwrap() {
-                        window.hide().unwrap();
-                    } else {
-                        window.show().unwrap();
-                        window.set_focus().unwrap();
-                    }
-                }
-                SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                    "quit" => {
-                        std::process::exit(0);
-                    }
-                    _ => {}
-                },
-                _ => {}
-            }
-        })
+        //     Ok(())
+        // })
+        // .on_system_tray_event(|app, event| {
+        //     tauri_plugin_positioner::on_tray_event(app, &event);
+        //     match event {
+        //         SystemTrayEvent::LeftClick {
+        //             position: _,
+        //             size: _,
+        //             ..
+        //         } => {
+        //             let window = app.get_window("main").unwrap();
+        //             let _ = window.move_window(Position::TrayCenter);
+        //             if window.is_visible().unwrap() {
+        //                 window.hide().unwrap();
+        //             } else {
+        //                 window.show().unwrap();
+        //                 window.set_focus().unwrap();
+        //             }
+        //         }
+        //         SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+        //             "quit" => {
+        //                 std::process::exit(0);
+        //             }
+        //             _ => {}
+        //         },
+        //         _ => {}
+        //     }
+        // })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
